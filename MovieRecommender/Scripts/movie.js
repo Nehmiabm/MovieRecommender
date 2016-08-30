101add1f352d7b51bf29043ef3d904390da6393b
Betsu <nehmiabm@gmail.com>
2016-08-30 14:12:06 -0500

    Restructured UI files

---
+++ MovieRecommender/Scripts/movie.js
@@ -0,0 +1,35 @@
+﻿
+$(document).ready(function () {
+
+    $("#recommendMe").click(function () {
+        var userid = $("#userId").val();
+        var table = $("#recomMovies").DataTable({
+            ajax: {
+                url: "/api/Movies/recommendations/" + userid,
+                dataSrc: ""
+            },
+            columns: [
+                {
+                    data: "title",
+                    render: function (data, type, recommend) {
+                        return "<a target='_blank' href='http://www.imdb.com/title/tt" + recommend.movieId + "'>" + recommend.title + "</a>";
+                    }
+                },
+
+                {
+                    data: "genre",
+                    render: function (data) {
+                        return data;
+                    }
+                },
+                 {
+                     data: "year",
+                     render: function (data) {
+                         return data;
+                     }
+                 }
+            ]
+        });
+    })
+
+});
